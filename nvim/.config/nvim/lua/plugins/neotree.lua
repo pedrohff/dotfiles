09be986b92d6b77d7110f13bf801078458d836e4
Pedro Feitosa <pedrohenriqueff@gmail.com>
2026-01-07 17:10:06 -0300

    chore(nvim): adds lazygit and neotree integration

---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+        window = {
+            mappings = {
+                ["["] = "expand_all_subnodes",
+                ["]"] = "close_all_nodes"
+            }
+        },
+        filesystem = {
+            position = "current",
+            hijack_netrw_behavior = "open_current",
+            -- "open_default",
+            -- "disabled",
+            filtered_items = {
+                -- when true, they will just be displayed differently than normal items
+                visible = true,
+                -- whether children of filtered parents should inherit their parent's highlight group
+                children_inherit_highlights = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_ignored = false, -- hide files that are ignored by other gitignore-like files
+                -- other gitignore-like files, in descending order of precedence.
+                ignore_files = {
+                    ".neotreeignore",
+                    ".ignore",
+                    -- ".rgignore"
+                },
+                hide_hidden = true, -- only works on Windows for hidden files/directories
+                hide_by_name = {
+                    ".DS_Store",
+                    "thumbs.db",
+                    --"node_modules",
+                },
+                hide_by_pattern = {
+                    --"*.meta",
+                    --"*/src/*/tsconfig.json",
+                },
+                always_show = { -- remains visible even if other settings would normally hide it
+                    --".gitignored",
+                },
+                always_show_by_pattern = { -- uses glob style patterns
+                    --".env*",
+                },
+                never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                    --".DS_Store",
+                    --"thumbs.db",
+                },
+                never_show_by_pattern = { -- uses glob style patterns
+                    --".null-ls_*",
+                },
+            },
+        }
+
+    }
+
+  }
+}
