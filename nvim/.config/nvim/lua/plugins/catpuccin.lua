081dfdedf4eacdc1d85093dc77a60dde6f62fe1b
Pedro Feitosa <pedrohenriqueff@gmail.com>
2026-01-07 15:35:57 -0300

    chore(nvim): removes lazyvim and sets up with only lazy.nvim package manager

--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme "catppuccin-mocha"
+--vim.cmd.colorscheme "catppuccin"
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+     { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
