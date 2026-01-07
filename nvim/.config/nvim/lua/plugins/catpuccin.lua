b0fd189d52bf9f1406ef010486f51d3be11335ed
Pedro Feitosa <pedrohenriqueff@gmail.com>
2026-01-07 20:31:06 -0300

    fix(nvim): themes and coloring settings

--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -1,4 +1,11 @@
---vim.cmd.colorscheme "catppuccin"
 return {
-     { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    {
+        "catppuccin/nvim", 
+        name = "catppuccin", 
+        priority = 1000 , 
+        opts = {
+            colorscheme = "catpuccin-mocha"
+        }
+
+    }
 }
