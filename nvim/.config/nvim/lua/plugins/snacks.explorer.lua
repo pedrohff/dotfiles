7ac540cc2c3f6dcbd4685a6d45e79c76e6a81759
Pedro Feitosa <pedrohenriqueff@gmail.com>
2026-01-07 14:51:52 -0300

    chore(nvim): setup file explorer with snacks.nvim

---
+++ nvim/.config/nvim/lua/plugins/snacks.explorer.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            -- explorer = {
+            -- },
+            picker = {
+                sources = {
+                    explorer = {
+                        auto_close = true,
+                        hidden=true,
+                        layout = {
+                            fullscreen = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
