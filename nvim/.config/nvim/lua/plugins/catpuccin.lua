5cf08a9f612ff7c25858245fec1bded6299e2f8b
Pedro Henrique Fontes Feitosa <pedrohenriqueff@gmail.com>
2026-01-09 15:11:10 -0300

    fix(nvim): catppuccin colorscheme typo

--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -1,11 +1,10 @@
 return {
-    {
-        "catppuccin/nvim", 
-        name = "catppuccin", 
-        priority = 1000 , 
-        opts = {
-            colorscheme = "catpuccin-mocha"
-        }
-
-    }
+     {
+         "catppuccin/nvim", 
+         name = "catppuccin", 
+         priority = 1000 , 
+         opts = {
+             colorscheme = "catppuccin-mocha"
+         }
+     }
 }
