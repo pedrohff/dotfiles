0ef43c032b320ed3b56bd8eaa7ee5e9cfa68ff36
Pedro Feitosa <pedrohff@verily.com>
2026-02-11 20:29:44 -0300

    chore(nvim): removing buggy `go.nvim` and going back to `fatih/vim-go`

--- nvim/.config/nvim/lua/plugins/gonvim.lua
+++ nvim/.config/nvim/lua/plugins/gonvim.lua
@@ -2,5 +2,36 @@ return {
     {
         "fatih/vim-go",
         build = ":GoInstallBinaries",
-    }
+    },
+    -- {
+    --     "ray-x/go.nvim",
+    --     dependencies = {  -- optional packages
+    --         "ray-x/guihua.lua",
+    --         "neovim/nvim-lspconfig",
+    --         "nvim-treesitter/nvim-treesitter",
+    --     },
+    --     opts = function()
+
+    --         require("go").setup(opts)
+    --         -- local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    --         -- vim.api.nvim_create_autocmd("BufWritePre", {
+    --         --     pattern = "*.go",
+    --         --     callback = function()
+    --         --         require('go.format').goimports()
+    --         --     end,
+    --         --     group = format_sync_grp,
+    --         -- })
+    --         return {
+    --             -- lsp_keymaps = true,
+    --             -- other options
+    --             run_in_floaterm = true,
+    --             --test_runner = 'gotestsum',
+    --             lsp_cfg = true,
+    --             lsp_on_attach = true,
+    --         }
+    --     end,
+    --     event = {"CmdlineEnter"},
+    --     ft = {"go", 'gomod'},
+    --     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    -- }
 }
