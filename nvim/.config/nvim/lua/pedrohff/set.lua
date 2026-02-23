871f96c33b55f71c03d3b6ed6f078d57767d14a1
Pedro Feitosa <pedrohff@verily.com>
2026-02-23 11:54:55 -0300

    chore(nvim): lsp & styling updates

--- nvim/.config/nvim/lua/pedrohff/set.lua
+++ nvim/.config/nvim/lua/pedrohff/set.lua
@@ -37,3 +37,31 @@ vim.cmd.colorscheme("catppuccin-mocha")
 
 vim.opt.wrap = true
 vim.opt.cmdheight = 0
+
+vim.lsp.enable({
+    "gopls",
+    "lua_ls"
+})
+vim.diagnostic.config({
+    --virtual_lines = true,
+    virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -5,6 +5,15 @@ return {
          priority = 1000 , 
          opts = {
              colorscheme = "catppuccin-mocha"
-         }
+         },
+         config = function()
+             require("catppuccin").setup({
+                 flavour="mocha",
+                 term_colors = true,
+                 background={
+                     dark = "mocha",
+                 },
+             })
+         end,
      }
 }
